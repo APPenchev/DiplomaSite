26225036b6122d28ee07d8ad3020af964c11796c
Penchev <36088861+APPenchev@users.noreply.github.com>
2024-12-09 18:12:10 +0200

    Initial Commit

---
+++ src/main/java/com/example/DiplomaSite/DiplomaSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.DiplomaSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomaSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DiplomaSite/DiplomaSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DiplomaSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
